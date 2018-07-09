13a6202b1f386548b35febfdfe1f8125b54a0b29
zualex <Azubarev32@yandex.ru>
2018-07-09 22:08:13 +0300

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+$env:FLASK_APP = "finance"
+$env:FLASK_ENV = "development"
+python -m flask run
+
+python -m flask init-db
+
+
+python -m pytest
+python -m coverage run -m pytest
+python -m coverage html
---
+++ finance/transaction.py
@@ -0,0 +1,15 @@
+import functools
+
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from flaskr.db import get_db
+
+bp = Blueprint('transaction', __name__, url_prefix='/')
+
+
+@bp.route('/')
+def index():
+    return render_template('transaction/index.html')
