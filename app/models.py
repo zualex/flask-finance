678c4e4c95ec685f0ad18f4bc763b69cff73ed0e
zualex <Azubarev32@yandex.ru>
2018-07-15 21:52:19 +0300

    migration currencies

--- app/models.py
+++ app/models.py
@@ -8,4 +8,12 @@ class Category(db.Model):
     parent_id = db.Column(db.Integer, db.ForeignKey('category.id'))
 
     def __repr__(self):
-        return '<User {}>'.format(self.username)
+        return '<Category {}>'.format(self.name)
+
+class Currency(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    symbol = db.Column(db.String(6), index=True, unique=True)
+    name = db.Column(db.String(64), index=True, unique=True)
+
+    def __repr__(self):
+        return '<Currency {}>'.format(self.name)
