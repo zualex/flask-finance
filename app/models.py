ea13fa44fc310610f147662fce4271334fe2adc0
zualex <Azubarev32@yandex.ru>
2018-07-15 22:00:22 +0300

    account migration

--- app/models.py
+++ app/models.py
@@ -17,3 +17,15 @@ class Currency(db.Model):
 
     def __repr__(self):
         return '<Currency {}>'.format(self.name)
+
+class Account(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), index=True, unique=True)
+    balance = db.Column(db.Float())
+    init_balance = db.Column(db.Float())
+    is_active = db.Column(db.Boolean())
+    is_ignore_balance = db.Column(db.Boolean())
+    currency_id = db.Column(db.Integer, db.ForeignKey('currency.id'))
+
+    def __repr__(self):
+        return '<Account {}>'.format(self.username)
