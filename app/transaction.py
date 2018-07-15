a36a3c014ccf7b86692ea5f0d7b5150a8c781963
zualex <Azubarev32@yandex.ru>
2018-07-15 21:47:03 +0300

    migration categories

--- app/transaction.py
+++ app/transaction.py
@@ -5,7 +5,7 @@ from flask import (
 )
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from app.models import User
+# from app.models import User
 from app import db
 
 bp = Blueprint('transaction', __name__, url_prefix='/')
