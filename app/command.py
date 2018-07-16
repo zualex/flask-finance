de221cc9f084d03d90f99c6ceff8ef832ae53444
zualex <Azubarev32@yandex.ru>
2018-07-16 16:07:31 +0300

    accounts seed

--- app/command.py
+++ app/command.py
@@ -4,6 +4,7 @@ from flask.cli import with_appcontext
 from app.models import Currency, Category, Account
 from app.seeds.currencies import Currencies
 from app.seeds.categories import Categories
+from app.seeds.accounts import Accounts
 from app import db
 
 
@@ -15,6 +16,7 @@ def init_app(app):
 @with_appcontext
 def seed_command():
     Currency.query.delete()
+    click.echo('-----------------')
     click.echo('Remove currencies')
     for data in Currencies.data:
         obj = Currency(**data)
@@ -23,11 +25,21 @@ def seed_command():
         click.echo('Currency ' + obj.symbol + ' created')
 
     Category.query.delete()
+    click.echo('-----------------')
     click.echo('Remove categories')
     for data in Categories.data:
         obj = Category(**data)
         db.session.add(obj)
         db.session.commit()
         click.echo('Category ' + obj.name + ' created')
+
+    Account.query.delete()
+    click.echo('---------------')
+    click.echo('Remove accounts')
+    for data in Accounts.data:
+        obj = Account(**data)
+        db.session.add(obj)
+        db.session.commit()
+        click.echo('Account ' + obj.name + ' created')
 
 
---
+++ app/seeds/accounts.py
@@ -0,0 +1,12 @@
+class Accounts(object):
+    data = [
+        {
+            'id': 1,
+            'name': 'Main',
+            'balance': 0,
+            'init_balance': 0,
+            'is_active': 1,
+            'is_ignore_balance': 0,
+            'currency_id': 1,
+        }
+    ]
