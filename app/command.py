1be3c8d27ce0daec1332136b6726019a6755522e
zualex <Azubarev32@yandex.ru>
2018-07-16 15:59:39 +0300

    categories seed

--- app/command.py
+++ app/command.py
@@ -3,6 +3,7 @@ from flask import current_app, g
 from flask.cli import with_appcontext
 from app.models import Currency, Category, Account
 from app.seeds.currencies import Currencies
+from app.seeds.categories import Categories
 from app import db
 
 
@@ -15,12 +16,18 @@ def init_app(app):
 def seed_command():
     Currency.query.delete()
     click.echo('Remove currencies')
-
     for data in Currencies.data:
         obj = Currency(**data)
         db.session.add(obj)
         db.session.commit()
         click.echo('Currency ' + obj.symbol + ' created')
 
+    Category.query.delete()
+    click.echo('Remove categories')
+    for data in Categories.data:
+        obj = Category(**data)
+        db.session.add(obj)
+        db.session.commit()
+        click.echo('Category ' + obj.name + ' created')
 
 
---
+++ app/seeds/categories.py
@@ -0,0 +1,123 @@
+class Categories(object):
+    data = [
+        {
+            'id': 1,
+            'name': 'Children',
+        },
+        {
+            'id': 2,
+            'name': 'Education',
+        },
+        {
+            'id': 3,
+            'name': 'Bills',
+        },
+            {
+                'id': 4,
+                'name': 'Internet',
+                'parent_id': 3,
+            },
+            {
+                'id': 5,
+                'name': 'Mortgage',
+                'parent_id': 3,
+            },
+            {
+                'id': 6,
+                'name': 'Credit',
+                'parent_id': 3,
+            },
+            {
+                'id': 7,
+                'name': 'Utility bills',
+                'parent_id': 3,
+            },
+            {
+                'id': 8,
+                'name': 'Insurance',
+                'parent_id': 3,
+            },
+            {
+                'id': 9,
+                'name':'Phone',
+                'parent_id': 3,
+            },
+        {
+            'id': 10,
+            'name':'Eating out',
+        },
+        {
+            'id': 11,
+            'name':'Food',
+        },
+        {
+            'id': 12,
+            'name':'Transport',
+        },
+            {
+                'id': 13,
+                'name':'Fuel',
+                'parent_id': 12,
+            },
+            {
+                'id': 14,
+                'name':'Public transport',
+                'parent_id': 12,
+            },
+            {
+                'id': 15,
+                'name':'Service',
+                'parent_id': 12,
+            },
+            {
+                'id': 16,
+                'name':'Taxi',
+                'parent_id': 12,
+            },
+        {
+            'id': 17,
+            'name':'Income',
+        },
+        {
+            'id': 18,
+            'name':'Health',
+        },
+        {
+            'id': 19,
+            'name':'Sport',
+        },
+        {
+            'id': 20,
+            'name':'Transfer',
+        },
+        {
+            'id': 21,
+            'name':'Gift',
+        },
+        {
+            'id': 22,
+            'name':'Charity',
+        },
+        {
+            'id': 23,
+            'name':'Purchases',
+        },
+            {
+                'id': 24,
+                'name':'Clothing',
+                'parent_id': 23,
+            },
+            {
+                'id': 25,
+                'name':'Electronics',
+                'parent_id': 23,
+            },
+        {
+            'id': 26,
+            'name':'Vacation',
+        },
+        {
+            'id': 27,
+            'name':'Others',
+        },
+    ]
